5d461f7412620bdc3216aed42735fdffc007c18b
vampire-yuta <yuta@vamdemicsystem.black>
2024-01-06 01:46:20 +0900

    init

---
+++ go.mod
@@ -0,0 +1,8 @@
+module proto-lesson
+
+go 1.20
+
+require (
+	github.com/gogo/protobuf v1.3.2
+	google.golang.org/protobuf v1.32.0
+)
---
+++ main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"proto-lesson/pb"
+
+	"github.com/gogo/protobuf/jsonpb"
+)
+
+func main() {
+	employee := &pb.Employee{
+		Id:          1,
+		Name:        "Sam",
+		Email:       "test@aaa.com",
+		Occupation:  pb.Occupation_ENGINEER,
+		PhoneNumber: []string{"000-0000-0000", "000-0000-0000"},
+		Project:     map[string]*pb.Company_Project{"ProjectX": &pb.Company_Project{}},
+		Profile: &pb.Employee_Text{
+			Text: "My name is Sam.",
+		},
+	}
+
+	// binData, err := proto.Marshal(employee)
+	// if err != nil {
+	// 	log.Fatal("marshaling error: ", err)
+	// }
+
+	// if err := ioutil.WriteFile("test.bin", binData, 0666); err != nil {
+	// 	log.Fatal("file write error: ", err)
+	// }
+
+	// in, err := ioutil.ReadFile("test.bin")
+	// if err != nil {
+	// 	log.Fatal("file read error: ", err)
+	// }
+
+	// readEmployee := &pb.Employee{}
+
+	// err = proto.Unmarshal(in, readEmployee)
+	// if err != nil {
+	// 	log.Fatal("unmarshaling error: ", err)
+	// }
+
+	// fmt.Println(readEmployee)
+
+	m := jsonpb.Marshaler{}
+
+	out, err := m.MarshalToString(employee)
+	if err != nil {
+		log.Fatal("marshaling error: ", err)
+	}
+
+	fmt.Println(out)
+}
